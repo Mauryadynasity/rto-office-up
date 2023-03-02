12cb2c3767a82ff3bc5237b797f8709e2471e5d2
Mauryadynasity <sunilm121296@gmail.com>
2023-03-02 21:55:07 +0530

    changes

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use DB;
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $sql = file_get_contents(database_path() . '/seeders/sql/admin.sql');
+        DB::statement($sql);
+    }
+}
---
+++ database/seeders/sql/admin.sql
@@ -0,0 +1,4 @@
+INSERT INTO `admin` (`id`, `name`, `email`, `password`, `role`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'Super Admin', 'admin@gmail.com','$2y$10$4itiWVpdUn5upxxAigIfH.PnQJIAkFqo.VUvMKuFMQZZPiepmSbiS', 1, '2023-02-26 05:42:58', '2023-02-26 05:42:58', NULL),
+(2, 'HO', 'ho@gmail.com','$2y$10$4itiWVpdUn5upxxAigIfH.PnQJIAkFqo.VUvMKuFMQZZPiepmSbiS', 2, '2023-02-26 05:42:58', '2023-02-26 05:42:58', NULL),
+(3, 'HO1', 'ho1@gmail.com','$2y$10$4itiWVpdUn5upxxAigIfH.PnQJIAkFqo.VUvMKuFMQZZPiepmSbiS', 3, '2023-02-26 05:42:58', '2023-02-26 05:42:58', NULL);
